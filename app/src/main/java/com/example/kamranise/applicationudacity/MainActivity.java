153132775ec0f94f74a0c38f036080ac7a702012
kamaran-sabr <kamaran.92sabr@gmail.com>
2017-07-16 13:48:55 +0300

    first commit

--- app/src/main/java/com/example/kamranise/applicationudacity/MainActivity.java
+++ app/src/main/java/com/example/kamranise/applicationudacity/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.kamranise.applicationudacity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
   TextView textView;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
                 "Tue, May 24 - Meatballs - 16°C / 18°C",
                 "Wed, May 25 - Cloudy - 19°C / 15°C",
                 "Thu, May 26 - Stormy - 30°C / 11°C",
-                "Fri, May 27 - Hurricane - 21°C / 9°C",
+              "Fri, May 27 - Hurricane - 21°C / 9°C",
                 "Sat, May 28 - Meteors - 16°C / 7°C",
                 "Sun, May 29 - Apocalypse - 16°C / 8°C",
                 "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
